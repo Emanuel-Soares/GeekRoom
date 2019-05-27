6ad0b767ca61cc2d461756b505ab44332a2b32ef
Emanuel Soares <emanuelsevero869@gmail.com>
2019-05-27 20:07:46 -0300

    iniciei o corpo do index

---
+++ main.php
@@ -0,0 +1,6 @@
+<div class="jumbotron" style="padding:120px;">
+    <div class="container-fluid">
+        <h1 class="display-4">Use nossa própria rede social</h1>
+        <a href="#" class="lead">Saiba mais</a>
+    </div>
+</div>
